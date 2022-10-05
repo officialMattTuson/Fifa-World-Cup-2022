7cf4edb1891ef0554ce9c3aed2ae62a1c61f188d
cornrowsMatt <mtuson1414@gmail.com>
2022-10-06 11:31:55 +1300

    removed excess and unnecessary files from repo

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 const express = require('express')
 const path = require('path')
 
-const fruitRoutes = require('./routes/fruits')
-
 const server = express()
 
 server.use(express.json())
@@ -11,7 +9,6 @@ server.use(express.static(path.join(__dirname, 'public')))
 server.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'))
   })
-  
-server.use('/api/v1/fruits', fruitRoutes)
+
 
 module.exports = server
